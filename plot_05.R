b7fc6af52adf0102b5a14e9276d09bcc8cccc1b0
John Brennan <johnneyb007@hotmail.com>
2015-10-11 22:57:21 +0100

    London Town 11th October Final Commit

--- plot_05.R
+++ plot_05.R
@@ -14,7 +14,7 @@ subMetering2 <- as.numeric(subSetData$Sub_metering_2)
 subMetering3 <- as.numeric(subSetData$Sub_metering_3)
 
 
-png("plot4.png", width=480, height=480)
+png("plot_04.png", width=480, height=480)
 par(mfrow = c(2, 2)) 
 
 plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power", cex=0.2)
